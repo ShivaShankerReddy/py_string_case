be610e99ab9890efa5f686c969ef65ec72c732f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-30 16:17:43 +0000

    Deploy ShivaShankerReddy/py_string_case to github.com/ShivaShankerReddy/py_string_case.git:gh-pages

---
+++ test.py
@@ -0,0 +1,17 @@
+import pytest
+from main import *
+
+def test_upper_case():
+    assert uppercase("hello") == "HELLO"
+
+
+def test_lower_case():
+    assert lowercase("Hello") == "hello"
+
+
+# def test_capital_case():
+    assert capitalcase("hello") == "Hello"
+
+
+# def test_capital_case():
+#     # assert 
