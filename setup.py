47f3acfbb2a2c910d0236d357692eb687f81186b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-03 13:30:51 +0000

    Deploy ShivaShankerReddy/py_string_case to github.com/ShivaShankerReddy/py_string_case.git:gh-pages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 import os
 
 
@@ -9,36 +9,22 @@ with open(os.path.join(rootdir, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-# setup(
-#     name='py_string_case',
-#     version='1.0.0',
-#     py_modules=[
-#         'py_string_case'
-#     ],
-#     url='https://github.com/ShivaShankerReddy/py_string_case',
-#     license='MIT',
-#     author='Shiva',
-#     author_email='shivashanker.chagamreddy@gmail.com',
-#     description='String case converter..',
-#     long_description=open('./README.rst').read()
-# )
-
-
 setup(
-    name="py_string_case",
-    version="0.0.1",
+    name="py_str_case",
+    version="0.0.4",
     author="shiva",
     author_email="shivashanker.chagamreddy@gmail.com",
     description="String case converter",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ShivaShankerReddy/py_string_case",
-    # packages=['main'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    scripts= ['src/main.py'],
+    py_modules=[
+        'py_str_case'
+    ],
     python_requires='>=3.0',
 )
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import pytest
-from main import *
+from py_str_case import *
 
 def test_upper_case():
     assert uppercase("hello") == "HELLO"
@@ -9,9 +9,45 @@ def test_lower_case():
     assert lowercase("Hello") == "hello"
 
 
-# def test_capital_case():
+def test_capital_case():
     assert capitalcase("hello") == "Hello"
 
 
-# def test_capital_case():
-#     # assert 
+def test_const_case():
+    assert constcase("foo_bar") == "FOO_BAR"
+
+
+def test_pascal_case():
+    assert pascalcase("foo_bar") == "Foobar"
+
+
+def test_path_case():
+    assert pathcase("foo_bar") == "foo/bar"
+
+
+def test_sentence_case():
+    assert sentencecase("FooBar") == "Foo bar"
+
+
+def test_snake_case():
+    assert snakecase("fooBarBaz") == "foo_bar_baz"
+
+
+def test_spinal_case():
+    assert spinalcase("foo_bar_baz") == "foo-bar-baz"
+
+
+def test_title_case():
+    assert titlecase("foo_bar") == "Foo Bar"
+
+
+def test_trim_case():
+    assert trimcase(" hello ") == "hello"
+
+
+def test_alphanum_case():
+    assert alphanumcase("Foo_123 Bar!") == "Foo123Bar"
+
+
+def test_fail_lower_case():
+    assert lowercase("") == "Enter the string to convert.."
